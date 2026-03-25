27a398c14c3262b6a0270623b998e534355e18e1
Rob Gage <rmg@oakleycomputing.org>
2026-03-25 17:52:40 -0400

    Implement construction and conversion macros for booleans

--- runtime/include/booleans.h
+++ runtime/include/booleans.h
@@ -3,10 +3,14 @@
 #ifndef BOOLEANS_H
 #define BOOLEANS_H
 
+#include "values.h"
+
 typedef char boolean_t;
 
-#define FALSE 0;
+#define BOOLEAN_FROM_VALUE(value) \
+    ((boolean_t)(VALUE_TO_INTEGER(value) != 0))
 
-#define TRUE 1;
+#define BOOLEAN_TO_VALUE(value) \
+    VALUE_FROM_INTEGER(BOOLEAN, (int64_t)(value))
 
 #endif
