b7c3588e46fe6011b8c2702db9678dec4c1d5ae5
Rob Gage <rmg@oakleycomputing.org>
2026-03-25 01:30:34 -0400

    Create lambdas.h

---
+++ runtime/include/lambdas.h
@@ -0,0 +1,8 @@
+// Copyright Rob Gage 2026
+
+#ifndef LAMBDAS_H
+#define LAMBDAS_H
+
+typedef struct lambda_t lambda_t;
+
+#endif LAMBDAS_H
---
+++ runtime/src/lambdas.c
@@ -0,0 +1,9 @@
+// Copyright Rob Gage 2026
+
+#include <stddef.h>
+#include <stdint.h>
+
+struct lambda_t {
+    size_t size;
+    instructions uint32_t[];
+};
