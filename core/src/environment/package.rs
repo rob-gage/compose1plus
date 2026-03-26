57a811f0606f6cff7cc83dbf9664f25428b17dfa
Rob Gage <rmg@oakleycomputing.org>
2026-03-26 00:13:13 -0400

    Implement `as_c_statement` method for `Combinator`

--- core/src/environment/package.rs
+++ core/src/environment/package.rs
@@ -2,10 +2,28 @@
 
 use crate::terms::Term;
 use super::Symbol;
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    fmt::{
+        Result as FmtResult,
+        Write,
+    }
+};
 
 /// A module containing valid functions
 pub struct Package {
     function_bodies: Box<[Box<[Term]>]>,
     exports: HashMap<Box<str>, Symbol>,
+}
+
+impl Package {
+
+    /// Generates C function declarations for this `Package`
+    fn generate_c_declarations<W: Write>(&self, w: &mut W) -> FmtResult {
+        for i in 0..self.function_bodies.len() {
+            writeln!(w, "\nvoid f{}(context_t*);", i)?;
+        }
+        Ok (())
+    }
+
 }
--- core/src/terms/combinator.rs
+++ core/src/terms/combinator.rs
@@ -1,8 +1,64 @@
 // Copyright Rob Gage 2026
 
 /// A concatenative combinator
+#[derive(Clone)]
 pub enum Combinator {
+    // stack combinators
     Copy,
     Drop,
     Hop,
+    Swap,
+    // lambda combinators
+    Apply,
+    Under,
+    Deep,
+    Compose,
+    Lift,
+    // arithmetic operators
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Remainder,
+    // boolean combinators
+    Not,
+    And,
+    Or,
+    Xor,
+    // comparison combinators
+    Equal,
+    Greater,
+    Less,
+}
+
+impl Combinator {
+
+    /// Returns this `Combinator` as a `C` statement to be used in function definitions
+    pub const fn as_c_statement(self) -> &'static str {
+        use Combinator::*;
+        match self {
+            Copy        => "k_copy(context);",
+            Drop        => "k_drop(context);",
+            Hop         => "k_hop(context);",
+            Swap        => "k_swap(context);",
+            Apply       => "k_apply(context);",
+            Under       => "k_under(context);",
+            Deep        => "k_deep(context);",
+            Compose     => "k_compose(context);",
+            Lift        => "k_lift(context);",
+            Add         => "k_add(context);",
+            Subtract    => "k_subtract(context);",
+            Multiply    => "k_multiply(context);",
+            Divide      => "k_divide(context);",
+            Remainder   => "k_remainder(context);",
+            Not         => "k_not(context);",
+            And         => "k_and(context);",
+            Or          => "k_or(context);",
+            Xor         => "k_xor(context);",
+            Equal       => "k_equal(context);",
+            Greater     => "k_greater(context);",
+            Less        => "k_less(context);",
+        }
+    }
+
 }
