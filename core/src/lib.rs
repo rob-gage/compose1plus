cb7bd7233fe8abbe347f3e688f60cbe8930e564c
Rob Gage <rmg@oakleycomputing.org>
2026-03-25 21:17:58 -0400

    Create `Term` enum

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,7 @@
 // Copyright Rob Gage 2026
 
 mod combinator;
+mod term;
 
 pub use combinator::Combinator;
+pub use term::Term;
---
+++ core/src/term.rs
@@ -0,0 +1,8 @@
+// Copyright Rob Gage 2026
+
+use crate::Combinator;
+
+/// A syntax term
+pub enum Term {
+    Combinator (Combinator),
+}
